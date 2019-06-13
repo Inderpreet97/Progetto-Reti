f08f7d926b47c587c47887212c103ea81c29fc06
GiuseppeUrbano <giuseppe.urbano@studenti.unipr.it>
2019-06-13 10:21:44 +0200

    Creazione progetto

---
+++ ProgettoRetiTLC/src/Server.java
@@ -0,0 +1,112 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Server {
+	
+	//Ports
+	private int sendPort = 4001;
+	private int receivePortServer = 4002;
+	private int receivePortClient = 4003;
+	
+	//UdpSockets
+	DatagramSocket udpReceive;
+	DatagramSocket udpSender;
+	DatagramPacket packet;
+	String currentdDestinationAddress = "";
+	
+	//Buffer receiver
+	byte[] buffer = new byte[1000];
+	
+	//Table
+	HashMap <String, String> table = new HashMap <String, String>();
+	
+	public void sendMessage(DatagramPacket packet, String destinationAddress) throws IOException {
+	
+		System.out.println("VOGLIO INVIARE");
+		
+		udpSender = new DatagramSocket();
+		
+		System.out.println("From: " +packet.getSocketAddress());
+		packet.setAddress(Inet4Address.getByName(destinationAddress));
+		packet.setPort(receivePortClient);
+		
+		//Sending packet
+		udpSender.send(packet);
+		udpSender.close();
+		
+		receiveMessage();
+		
+	}
+	
+	public void receiveMessage() throws IOException {
+		
+		udpReceive = new DatagramSocket(receivePortServer);
+		
+		packet = new DatagramPacket(buffer, buffer.length);
+		
+		System.out.println("------------- UDP SERVER -------------");
+		
+		while(true) {
+			
+			System.out.println("SONO IN RICEZIONE");
+			
+			//Receiver
+			udpReceive.receive(packet);
+			
+			//Getting informations from the packet received
+			String message = new String(packet.getData(), packet.getOffset(), packet.getLength());
+			String sourceAddress = packet.getAddress().getHostAddress()+":"+packet.getPort();
+			
+			
+			if (message.startsWith("Client hello")) {
+				//Printing message received
+				System.out.println("Client hello -> : From "+sourceAddress+": "+message);
+				boolean copy = false;
+				for (int i=0; i<message.length(); i++) {
+					//Get destination address
+					if (copy) currentdDestinationAddress += message.charAt(i);
+					if ( message.charAt(i) == ':') copy = true;	
+				}
+				
+				System.out.println("Destination address: " + currentdDestinationAddress);
+				table.put(sourceAddress, currentdDestinationAddress);
+				
+				
+			}else {
+				//Printing message received
+				
+				if (table.containsKey(sourceAddress)) {
+					//destinationAddress = table.get(sourceAddress);
+					System.out.println("Devo inviare a "+ table.get(sourceAddress));
+					udpReceive.close();
+					break;
+					
+				}else {
+					System.out.println("ERRORE");
+				}
+				
+			}
+			
+			
+		}
+		
+		sendMessage(packet, currentdDestinationAddress);
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		Server server = new Server();
+		server.receiveMessage();
+		//server.sendMessage("192.168.1.83");
+
+	}
+
+}
---
+++ ProgettoRetiTLC/src/UdpClient.java
@@ -0,0 +1,125 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class UdpClient {
+	
+	private String serverAddress = "192.168.1.137";
+	private String destinationAddress = "192.168.1.83";
+	
+	//Ports
+	private int sendPort = 4001;
+	private int receivePortServer = 4002;
+	private int receivePortClient = 4003;
+	
+	//Buffer receiver
+	byte[] buffer = new byte[1000];
+	
+	//Constructor
+	public UdpClient() {
+	}
+	
+	public void Menu() throws IOException{
+		
+		System.out.println("Comando: ");
+		
+		String option = new Scanner(System.in).next();
+		
+		while(!option.equals("close")) {
+			option = option.toLowerCase();
+			
+			switch(option) {
+			case "send":	case "sender":		sendMessage();		break;
+			case "receive":						receiveMessage();	break;
+			}
+			
+			option = new Scanner(System.in).next();
+			
+		}
+		
+	}
+	
+	public void sendMessage() throws IOException {
+		
+		System.out.println("------------- UDP SENDER -------------");
+		
+		// 1) Set recipient and Send a client hello to server with that particular recipient
+		// 2) Send message
+		// 3) Bye
+		
+		DatagramSocket udp = new DatagramSocket();
+		
+		// 1) Client hello
+		String message = "Client hello:" + destinationAddress;
+		byte[] data = message.getBytes();
+		
+		DatagramPacket clientHello = new DatagramPacket(data, data.length);
+		clientHello.setAddress(Inet4Address.getByName(serverAddress));
+		clientHello.setPort(receivePortServer);
+		udp.send(clientHello);
+						
+		
+		// 2) Messages
+		System.out.println("Type your text: ");
+		Scanner text = new Scanner(System.in);
+		message = text.nextLine();
+		
+		while(!message.equals("close")) {
+			
+			data = message.getBytes();
+			
+			//Set up packet
+			DatagramPacket packet = new DatagramPacket(data, data.length);
+			packet.setAddress(Inet4Address.getByName(serverAddress));
+			packet.setPort(receivePortServer);
+			
+			//Sending packet
+			udp.send(packet);
+			
+			//New message -- "close" to finish
+			text = new Scanner(System.in);
+			message = text.nextLine();
+			
+		}
+		
+		//3) Bye
+		message = "Bye: ";
+		
+	}
+	
+	public void receiveMessage() throws IOException {
+		
+		DatagramSocket udp = new DatagramSocket(receivePortClient);
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+		
+		System.out.println("------------- UDP RECEIVER -------------");
+		
+		while(true) {
+			
+			//Receiver
+			udp.receive(packet);
+			
+			//Getting informations from the packet received
+			String message = new String(packet.getData(), packet.getOffset(), packet.getLength());
+			
+			String sourceAddress = packet.getAddress().getHostAddress()+":"+packet.getPort();
+			
+			//Printing message received
+			System.out.println("From "+sourceAddress+": "+message);
+			
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+			
+			//Creating an instance of UdpClient class
+			UdpClient client = new UdpClient();
+			
+			//Running Client
+			client.Menu();
+		}
+}
