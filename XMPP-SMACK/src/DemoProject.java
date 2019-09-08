cfaa8037e21a51e8d9b8fb21ccdfb7d5730ca0ec
Inderpreet97 <inderpreet.singh1@studenti.unipr.it>
2019-09-08 14:40:18 +0200

    programma esempio funzionante

--- XMPP-SMACK/src/DemoProject.java
+++ XMPP-SMACK/src/DemoProject.java
@@ -1,4 +1,11 @@
-import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+import java.util.Scanner;
+
+import org.jivesoftware.smack.AccountManager;
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.packet.Message;
 
 public class DemoProject {
 
@@ -9,13 +16,40 @@ public class DemoProject {
 			public void run() {
 				
 				try {
+					ConnectionConfiguration config = new ConnectionConfiguration("127.0.0.1", 5222);
+					config.setSecurityMode(ConnectionConfiguration.SecurityMode.disabled);
+					config.setDebuggerEnabled(false);
+					config.setSendPresence(true);
+					
+					XMPPConnection con = new XMPPConnection(config);
+					con.connect();
+					
+					AccountManager manager = con.getAccountManager();
+					manager.createAccount("indismack", "provasmack");
+					
+					con.login("indismack", "provasmack");
+					
+					Chat chat = con.getChatManager().createChat("indi@desktop-qi7gbpd.lan", new MessageListener () {
 
+						@Override
+						public void processMessage(Chat chat, Message msg) {
+							System.out.println(chat.getParticipant() + " said: " + msg.getBody());
+							
+						}
+						
+					});
+					
+					System.out.println("Connected");
+					
+					Scanner reader = new Scanner(System.in);
+					while(con.isConnected()) {
+						chat.sendMessage(reader.nextLine());
+					}
+					reader.close();
 					
 				}
 				catch(Exception ex) {
-					
 					ex.printStackTrace();
-					
 				}
 			}
 		}.start();
