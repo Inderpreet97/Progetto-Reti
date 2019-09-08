e411fc0ee2eaaa6939526e1a6444fd1d5412c42d
Inderpreet97 <inderpreet.singh1@studenti.unipr.it>
2019-09-08 13:55:14 +0200

    Progetto di prova XMPP-SMACK

---
+++ XMPP-SMACK/src/DemoProject.java
@@ -0,0 +1,24 @@
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+
+public class DemoProject {
+
+	public static void main(String[] args) {
+		
+		new Thread() {
+			
+			public void run() {
+				
+				try {
+
+					
+				}
+				catch(Exception ex) {
+					
+					ex.printStackTrace();
+					
+				}
+			}
+		}.start();
+	}
+
+}
